6bb049a95f396322659aca0a15c777a9277041cd
Ilja Plotnikov <example@mail.com>
2024-01-14 14:31:21 +0200

    add my first app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// This is a comment
+
+
+using System.Xml.Linq;
+
+string MyName = "Harry";
+string MyLastName = "Potter";
+
+//Console.WriteLine("Hello" + " " + myName);
+
+Console.WriteLine($"Hello, {MyName} {MyLastName}!");
